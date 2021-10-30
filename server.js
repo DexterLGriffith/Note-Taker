0f8433fe1a0b9590afc87416c81b82fc7ca3362a
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-30 15:34:01 -0500

    still connecting to home page... but server is up.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//query posts
 //serve middleware
 //serve static public folder..
 app.use(express.static('public'));
@@ -15,4 +16,18 @@ app.use(express.json());
 app.get('/api/notes', (req, res) => {
     const readFile = JSON.parse(fs.readFile('db/db.jsona',{encoding: 'utf-8'}));
     res.json(readFile);
+});
+//htmlgetroute
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+//query post end..
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    //lets user know the port its running off of.
 });

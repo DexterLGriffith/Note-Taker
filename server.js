d91c3d8a5505bdb2ab2b1438e82456759d9814c5
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-30 16:41:01 -0500

    ASDF:LKASDFJA:SLKJ it isnt deleting :(

--- server.js
+++ server.js
@@ -13,24 +13,25 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 //setup routes
 app.get('/api/notes', (req, res) => {
-    const readFile = JSON.parse(fs.readFile('db/db.json',{encoding: 'utf-8'}));
+    const readFile = JSON.parse(fs.readFileSync('db/db.json',{encoding: 'utf-8'}));
     res.json(readFile);
 });
 app.post('/api/notes', (req, res) => {
-    const readFile = JSON.parse(fs.readFile('db/db.json',{encoding: 'utf-8'}));
+    const readFile = JSON.parse(fs.readFileSync('db/db.json',{encoding: 'utf-8'}));
     const newNote = {title: req.body.tile,text: req.body.text, id: nanoid()};
     readFile.push(newNote);
     fs.writeFileSync('db/db.json', JSON.stringify(readFile));
 });
 app.delete('/api/notes/:id', (req, res) => {
-    const readFile = JSON.parse(fs.readFile('db/db.json',{encoding: 'utf-8'}));
-    const newNote = []; 
+    let readFile = JSON.parse(fs.readFileSync('db/db.json',{encoding: 'utf-8'}));
+    let newNote = []; 
     newNote = readFile;
     const getID = req.params.id; 
     newNote = newNote.filter(({ id }) => id !== getID);
-    res.JSON(newNote)
+    res.json(newNote)
     fs.writeFileSync('db/db.json', JSON.stringify(readFile));
 })
 //htmlgetroutes
